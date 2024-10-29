25af10f3549d4434aa364940ab7cbeea4299c376
Andres Bonilla <149350065+Andres94b@users.noreply.github.com>
2024-10-29 16:01:17 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
+// Event handlers
 $(document).ready(function () {
     let history = [];
     let totalPrice = 0;
+	let taskGrid = [];
 
     // Add tasks
-
-
+	$('#add-task').click(function(){
+		window.open("task_popup.html","","width=600,height=250,left=400,top=200");
+	});
+	
+	$("#list-tasks").click(function(){
+		
+	});
+	
     // Add to Cart functionality for the entire product box
     $('.task').click(function () {
         const task = $(this);
@@ -20,6 +28,7 @@ $(document).ready(function () {
             history.push({ id: taskId, name: taskName, price: taskPrice, quantity: 1 });
         }
 
+		
         // Update cart UI
         updateHistory();
     });
@@ -35,28 +44,6 @@ $(document).ready(function () {
         updateHistory();
     });
 
-    // Update Cart
-    function updateHistory() {
-        const historyItemsContainer = $('.history-grid');
-        historyItemsContainer.empty();
-
-        totalPrice = 0;
-        history.forEach(item => {
-            totalPrice += item.price * item.quantity;
-
-            const historyItemHtml = `
-                <div class="history-item" data-id="${item.id}">
-                    <h3>${item.name}</h3>
-                    <p>Quantity: ${item.quantity}</p>
-                    <p>$${(item.price * item.quantity).toFixed(2)}</p>
-                </div>
-            `;
-            historyItemsContainer.append(historyItemHtml);
-        });
-
-        $('#total-price').text(totalPrice.toFixed(2));
-    }
-
     // Checkout button
     $('#checkout-button').click(function () {
         alert('Proceeding to checkout. Total: $' + totalPrice.toFixed(2));
@@ -64,6 +51,43 @@ $(document).ready(function () {
         updateHistory();  // Update UI
     });
 });
+
+// Support Functions
+
+// Update Cart
+function updateHistory() {
+    const historyItemsContainer = $('.history-grid');
+    historyItemsContainer.empty();
+
+    totalPrice = 0;
+    history.forEach(item => {
+        totalPrice += item.price * item.quantity;
+
+        const historyItemHtml = `
+            <div class="history-item" data-id="${item.id}">
+                <h3>${item.name}</h3>
+                <p>Quantity: ${item.quantity}</p>
+                <p>$${(item.price * item.quantity).toFixed(2)}</p>
+            </div>
+        `;
+        historyItemsContainer.append(historyItemHtml);
+    });
+
+    $('#total-price').text(totalPrice.toFixed(2));
+}
+
+function displayTasks(){
+	
+}
+
+function receiveTaskData(task, save) {
+			if (save) {
+			    saveToStorage(task, function () {
+					console.log("saved succesfully");
+			    });
+			}
+		}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
         .then((registration) => {
