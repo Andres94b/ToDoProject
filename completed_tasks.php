bb9018bbc8f3f3b0977c2c91d5a91e4dc394bda2
Joaquin Cervantes <149394966+JofeCC@users.noreply.github.com>
2024-10-29 18:40:18 -0400

    Update completed_tasks.php

--- completed_tasks.php
+++ completed_tasks.php
@@ -3,10 +3,10 @@ include 'dbconfig.php';
 
 
 $query = "SELECT * FROM tasks WHERE status='completed'";
-$result = mysqli_query($conn, $query);
+$result = mysqli_query($connection, $query);
 
 while($row = mysqli_fetch_assoc($result)) {
-    echo "<div class='task completed'>
+    echo "<div class='history-item completed'>
             <span>{$row['task_name']} - Completed at: {$row['completed_at']}</span>
           </div>";
 }
