e5bd3f79498cbfda64567cdf313bfc92e4106686
Joaquin Cervantes <149394966+JofeCC@users.noreply.github.com>
2024-10-29 17:55:57 -0400

    Update operationData.php

--- operationData.php
+++ operationData.php
@@ -1,8 +1,16 @@
 <?php
 include 'dbconfig.php';
 
-// $operation = $_GET["op"];
 
+// $operation = $_GET["op"];
+if (isset($_GET['edit'])) {
+    $task_id = $_GET['id'];
+    edit_task($task_id);     // Fetch the task data based on the task ID
+}
+
+if (isset($_GET['add'])) {
+    insert_task();     // Fetch the task data based on the task ID
+}
 
 // switch ($operation) {
 //     case "add_task":
@@ -20,25 +28,30 @@ include 'dbconfig.php';
 //     break;
 // }
 
-insert_task();
-
-
+// insert_task();
 
 function insert_task() {
     //$task_name = $_POST['task_name'];
     //$task_description = $_POST['task_description'];
-    global $conn;
+    global $connection;
     
     $task_name = $_POST["taskName"];
     $task_description = $_POST["taskDescription"];
     $task_status = $_POST["taskStatus"];
     
-    
     $query = "INSERT INTO tasks (task_name, task_description) VALUES ('$task_name', '$task_description')";
-    mysqli_query($conn, $query);
+    mysqli_query($connection, $query);
     
     //header('Location: index.php');
 }
 
+function edit_task($task_id) {
+    global $connection;
+    $task_name = $_POST["task_name"];
+    $task_description = $_POST["task_description"];
+    $task_status = $_POST["status"];
+    $query = "UPDATE tasks SET task_name = '$task_name', task_description = '$task_description', status = '$task_status' WHERE id = $task_id";
+    mysqli_query($connection, $query);
+}
 
 ?>
