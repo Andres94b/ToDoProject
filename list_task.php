7d6744c4fee4aba36836624a5eedce59fe0a0412
Joaquin Cervantes <149394966+JofeCC@users.noreply.github.com>
2024-10-29 17:55:23 -0400

    Update list_task.php

--- list_task.php
+++ list_task.php
@@ -1,15 +1,18 @@
 <?php
 include 'dbconfig.php';
-global $conn;
+global $connection;
 
 $query = "SELECT * FROM tasks WHERE status='pending'";
-$result = mysqli_query($conn, $query);
+$result = mysqli_query($connection, $query);
 
 while($row = mysqli_fetch_assoc($result)) {
     echo "<div class='task'>
-            <span>{$row['task_name']}</span>
+            <span>{$row['task_name']}</span><br/>
+            <span>{$row['task_description']}</span><br/>
+            <span>{$row['id']}</span>
+
             <div>
-                <a href='edit_task.php?id={$row['id']}'>Edit</a>
+                <a href='../edit_task.php?id={$row['id']}'>Edit</a>
                 <a href='complete_task.php?id={$row['id']}'>Complete</a>
                 <a href='delete_task.php?id={$row['id']}'>Delete</a>
             </div>
