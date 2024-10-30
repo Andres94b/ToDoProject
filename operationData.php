aa8ec9e50e1ec7ce684944f35f6ee45b21f0b99e
Pratiksha Dutta <146384410+Prats0509@users.noreply.github.com>
2024-10-29 23:27:27 -0400

    Update operationData.php

--- operationData.php
+++ operationData.php
@@ -1,61 +1,16 @@
 <?php
 include 'dbconfig.php';
-
-
-// $operation = $_GET["op"];
-if (isset($_GET['edit'])) {
-    $task_id = $_GET['id'];
-    edit_task($task_id);     // Fetch the task data based on the task ID
-}
-
+global $conn;
 if (isset($_POST['add'])) {
-    insert_task();
-    echo "inserted.";// Fetch the task data based on the task ID
+    include 'add_task.php';
+} elseif (isset($_GET['edit'])) {
+    include 'edit_task.php';
+} elseif (isset($_GET['delete'])) {
+    include 'delete_task.php';
+} elseif (isset($_GET['complete'])) {
+    include 'completed_tasks.php';
+} else {
+    echo "Invalid operation.";
 }
-
-// switch ($operation) {
-//     case "add_task":
-//         insert_task();
-//         echo "inserting...!";
-//     break;
-//     case "update_task":
-//         echo "updating...!";
-//         break;
-//     case "delete_task":
-//         echo "updating...!";
-//         break;
-//     default:
-//         echo "wrong option.";
-//     break;
-// }
-
-// insert_task();
-
-function insert_task() {
-    //$task_name = $_POST['task_name'];
-    //$task_description = $_POST['task_description'];
-    global $connection;
-    
-    $task_name = $_POST["taskName"];
-    $task_description = $_POST["taskDescription"];
-    $task_status = $_POST["taskStatus"];
-    
-    $query = "INSERT INTO tasks (task_name, task_description, status) VALUES ('$task_name', '$task_description', '$task_status')";
-    
-    try {
-        mysqli_query($connection, $query);
-    } catch (Exception $e) {echo "Error<br/>";
-    }
-    //header('Location: index.php');
-}
-
-function edit_task($task_id) {
-    global $connection;
-    $task_name = $_POST["task_name"];
-    $task_description = $_POST["task_description"];
-    $task_status = $_POST["status"];
-    $query = "UPDATE tasks SET task_name = '$task_name', task_description = '$task_description', status = '$task_status' WHERE id = $task_id";
-    mysqli_query($connection, $query);
-}
-
 ?>
+
